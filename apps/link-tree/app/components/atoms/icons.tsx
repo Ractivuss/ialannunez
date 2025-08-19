5cf484c7fae72f70dbce146bd73bb9d4b7dcfedc
Alan Núñez <nunezalan0798@gmail.com>
2025-08-18 19:31:41 -0600

    Refactored multiple components to use arrow function syntax for consistency and improved readability. Updated handleRequest, CardFlip, Header, ThemeForm, and various icon components accordingly. Ensured all functions are exported as constants to align with modern React practices.

--- apps/link-tree/app/components/atoms/icons.tsx
+++ apps/link-tree/app/components/atoms/icons.tsx
@@ -4,7 +4,7 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
   size?: number;
 }
 
-export function TwitterIcon({ size = 16, className, ...props }: IconProps) {
+export const TwitterIcon = ({ size = 16, className, ...props }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -18,9 +18,9 @@ export function TwitterIcon({ size = 16, className, ...props }: IconProps) {
       <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
     </svg>
   );
-}
+};
 
-export function GithubIcon({ size = 16, className, ...props }: IconProps) {
+export const GithubIcon = ({ size = 16, className, ...props }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -38,9 +38,9 @@ export function GithubIcon({ size = 16, className, ...props }: IconProps) {
       />
     </svg>
   );
-}
+};
 
-export function LinkedinIcon({ size = 16, className, ...props }: IconProps) {
+export const LinkedinIcon = ({ size = 16, className, ...props }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -54,9 +54,9 @@ export function LinkedinIcon({ size = 16, className, ...props }: IconProps) {
       <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z" />
     </svg>
   );
-}
+};
 
-export function GlobeIcon({ size = 16, className, ...props }: IconProps) {
+export const GlobeIcon = ({ size = 16, className, ...props }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -70,9 +70,9 @@ export function GlobeIcon({ size = 16, className, ...props }: IconProps) {
       <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
     </svg>
   );
-}
+};
 
-export function LinkIcon({ size = 16, className, ...props }: IconProps) {
+export const LinkIcon = ({ size = 16, className, ...props }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -86,4 +86,4 @@ export function LinkIcon({ size = 16, className, ...props }: IconProps) {
       <path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z" />
     </svg>
   );
-}
+};
